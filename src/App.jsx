8210a068f74775cfe6cce1274f6fad515e4819bb
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-16 23:32:21 +0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () =>  (
+    <div className="bg-secondary w-full overflow-hidden">
+      <h1>Hello </h1>
+    </div>
+  );
+
 
 export default App
