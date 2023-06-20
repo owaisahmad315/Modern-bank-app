dfcf2b46ebb484e4905518bc88424416e6a74141
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-20 23:31:31 +0500

    working on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react"
+
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="w-full flex py-6 justify-between item-center">
+      <img src={logo} alt="hoobank"
+      className="w-[124px] h-[32px]"
+      />
+
+      <ul>
+        
+      </ul>
+    </nav>
   )
 }
 
