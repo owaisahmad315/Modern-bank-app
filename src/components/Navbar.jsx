d6d60b57c2e25c65f69cf89467be7fa9ba435516
Owais Ahmad <owaisahmad014@gmail.com>
2023-06-21 23:47:02 +0500

    worked on navlinks for destop screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,26 @@ const Navbar = () => {
   return (
     <nav className="w-full flex py-6 justify-between item-center">
       <img src={logo} alt="hoobank"
-      className="w-[124px] h-[32px]"
-      />
+      className="w-[124px] h-[32px]"/>
 
-      <ul>
-        
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+          key={nav.id}
+          className={`font-poppins
+            font-normal
+            cursor-pointer
+            text-[16px]
+            text-white
+            ${index === navLinks.length -1 ? 'mr-0' : 'mr-10' }
+          `}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+
+          </li>
+        ))}
       </ul>
     </nav>
   )
