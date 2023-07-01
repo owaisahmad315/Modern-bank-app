c83cbe86e728525919c83b61ceac980b789ad398
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-01 23:34:19 +0500

    ...

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,17 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button'
 
+
+const FeatureCard = ({ icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px]
+    ${index !== features.length - 1 ? "mb-6" : "mb-0" } feature-card ` }>
+    <div>
+      <img src={icon} alt="icon" 
+      className="w-[50%] h-[50%] object-contain"/>
+    </div>
+  </div>
+)
+
 const Business = () => {
   return (
      <section id="features" className={layout.section}>
@@ -16,6 +27,13 @@ const Business = () => {
         </p>
 
         <Button styles="mt-10"/>
+      </div>
+      <div className={`${layout.sectionImg}
+      flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id}
+          {...feature} index={index}/>
+        ))}
 
       </div>
 
