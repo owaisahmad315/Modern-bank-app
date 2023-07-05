621821b42adeccfd27d30f54b356ed7b402c015a
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-05 17:27:04 +0500

    ...

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { quotes } from "../assets";
+import React from "react";
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
-  )
-}
+const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+   
+  </div>
+);
 
-export default FeedbackCard
+
+export default FeedbackCard;
