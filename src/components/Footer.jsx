8202910292668065abe89da8a60c64bfecea94ba
Owais Ahmad <owaisahmad014@gmail.com>
2023-07-09 16:48:08 +0500

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,7 @@ import { logo } from "../assets"
 import { footerLinks, socialMedia } from "../constants"
 
 const Footer = () => {
+   
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row
@@ -41,14 +42,31 @@ const Footer = () => {
                       {Link.name}
                   </li>
                 ))}
-
               </ul>
-
             </div>
+          ))}
+        </div>
+      </div>
+
+      <div  className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+        <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        2021 HooBank. All Rights Reserved.
+        </p>
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+            <img 
+                 key={social.id}
+                 src={social.icon}
+                 alt={social.id}
+                 className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+                  index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+                }`}
+            
+            />
+
           ))}
 
         </div>
-
       </div>
     </section>
   )
